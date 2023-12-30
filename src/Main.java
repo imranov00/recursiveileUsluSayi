1962491af339572693739d7ac2d778ba2d1616e4
İmranov <semtexpert@gmail.com>
2023-12-30 14:33:49 +0300

    version 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static int mod(int taban, int us) {
+        int result = 1;
+        for (int i = 1; i <= us; i++) {
+            result *= taban;
+
+
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner inp = new Scanner(System.in);
+
+
+        System.out.println("Taban Değerini Giriniz: ");
+        int taban= inp.nextInt();
+
+        System.out.println("Üssünü Giriniz: ");
+        int us= inp.nextInt();
+
+        System.out.println(mod(taban,us));
     }
 }
